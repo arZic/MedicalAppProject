41c810f7e169a00f0538d46464b26d5d163ba588
leroy.benjamin <benjamin.leroy73100@gmail.com>
2017-12-06 17:12:47 +0100

    Modification page création patient

--- app/src/main/java/com/btssio/leroybenjamin/medicalappproject/MainActivity.java
+++ app/src/main/java/com/btssio/leroybenjamin/medicalappproject/MainActivity.java
@@ -1,7 +1,12 @@
 package com.btssio.leroybenjamin.medicalappproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textviewCreer = (TextView) findViewById(R.id.textviewCreer);
+        TextView textviewVoir = (TextView) findViewById(R.id.textviewVoir);
+
+        textviewCreer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentPatient = new Intent(MainActivity.this, PatientActivity.class);
+                startActivity(intentPatient);
+            }
+        });
     }
 }

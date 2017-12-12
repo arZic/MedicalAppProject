96f0b717a5120a6e2b67313999f2752f1f2e3567
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2017-12-12 10:52:19 +0100

    Ajout de la liste de patients (java)

--- app/src/main/java/com/btssio/leroybenjamin/medicalappproject/PatientActivity.java
+++ app/src/main/java/com/btssio/leroybenjamin/medicalappproject/PatientActivity.java
@@ -1,7 +1,15 @@
 package com.btssio.leroybenjamin.medicalappproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class PatientActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class PatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+
+        ArrayList<Patient> lesPatients = new ArrayList<Patient>();
+        lesPatients.add(new Patient("Dupuis", "Joseph", new Date(20/12/1960), "C410", R.drawable.joseph));
+        lesPatients.add(new Patient("Dupont", "Claire", new Date(18/12/1997), "C412", R.drawable.claire));
+        lesPatients.add(new Patient("Champs", "Olivier", new Date(02/03/1978), "C413", R.drawable.olivier));
+
+        PatientAdapter adapterPatients = new PatientAdapter(this, lesPatients);
+        ListView listViewPatients = (ListView) findViewById(R.id.listView_patients);
+        listViewPatients.setAdapter(adapterPatients);
+
+        /* listViewPatients.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Patient monPatient;
+                monPatient = (Patient) adapterView.getItemAtPosition(i);
+                Intent intent = new Intent(PatientActivity.this, DetailsActivity.class);
+                intent.putExtra("Patient", (Serializable)monPatient);
+                startActivity(intent);
+            }
+        }); */
     }
 }
